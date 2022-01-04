d2c4d90d15d42644a63ad59181363ac221c955c2
J Lee <jay.j.lee.1@gmail.com>
2022-01-04 16:30:37 -0500

    Z assist

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const recipeBar = document.getElementById('recipeBar');
 const recipeBox = document.getElementById('recipeBox');
 const proteinBox = document.getElementById('proteinBox');
-
+const proteinInBox = document.querySelectorAll(".proteinInBox")
 // document.addEventListener('DOMContentLoaded', () => {
-//   proteinBox.addEventListener('click', (e)=> {
+//   proteinInBox.addEventListener('click', (e)=> {
 //   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.id}`)
 //     .then(res => res.json())
 //     .then(console.log)})})
   
 
-proteinBox.addEventListener('click', (e)=> {
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.id}`)
+document.querySelectorAll(".proteinInBox").forEach((ele) => {
+  ele.addEventListener('click', (e)=> { 
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.target.id}`)
     .then(res => res.json())
     .then(console.log)
+  })  
 })
-//     // .then((food) => food.forEach(showRecipeBar)))
-//     // .then(recipes => recipes.forEach(showRecipeBar))
-// )
+
 
 // function showRecipeBar(recipes) {
 //   // const span = document.createElement("span")
