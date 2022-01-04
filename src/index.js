2ee5d2e52b28bd07ae0352aef817aa4e4b542de3
J Lee <jay.j.lee.1@gmail.com>
2022-01-04 13:36:30 -0500

    no more slideshow

--- src/index.js
+++ src/index.js
@@ -1,36 +1,26 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+const recipeBar = document.getElementById('recipeBar');
+const recipeBox = document.getElementById('recipeBox');
+const proteinBox = document.getElementById('proteinBox');
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// document.addEventListener('DOMContentLoaded', () => {
+//   proteinBox.addEventListener('click', (e)=> {
+//   fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.id}`)
+//     .then(res => res.json())
+//     .then(console.log)})})
+  
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-// if(document.getElementsByClassName("protein").style.display = "block")
-// create function to fetch based on what is on block
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("protein");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-
-  let displayMeat = slides[slideIndex-1]
-  return fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${displayMeat.id}`)
+proteinBox.addEventListener('click', (e)=> {
+  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.id}`)
     .then(res => res.json())
     .then(console.log)
-}
+})
+//     // .then((food) => food.forEach(showRecipeBar)))
+//     // .then(recipes => recipes.forEach(showRecipeBar))
+// )
+
+// function showRecipeBar(recipes) {
+//   // const span = document.createElement("span")
+//   const pics = document.createElement('img')
+//   pics.src = recipes["strMealThumb"]
+//   console.log("pics showing?")
+//   recipeBar.appendChild(pics)}
