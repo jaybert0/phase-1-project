7906cdbca0bcd63daf40d67f8d61fe05e87c294a
J Lee <jay.j.lee.1@gmail.com>
2022-01-05 16:56:31 -0500

    Ratings and Comments working f yeah

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+function init() {
+  handleForm()
+  resetVoteCount()
+  enterComment()
+}
+init()
+
 // Global Variables
 const recipeBar = document.getElementById('recipeBar');
 const recipeBarSpan = document.getElementById('recipeBarSpan');
@@ -13,6 +20,7 @@ proteinInBox.forEach((ele) => {
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.target.id}`)
     .then(res => res.json())
     .then(food => food.meals.forEach(showRecipeBar))
+    .finally(removeAllChildNodes(recipeBarSpan))
     // e.target.reset()
   })  
 })
@@ -22,17 +30,48 @@ function removeAllChildNodes(parent) {
     }
 }
 
+function handleForm(){
+  document.getElementById('votes-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("Thanks for the rating")
+      let user_input = event.target.votes.value
+      // document.querySelector('#votes-form').innerText = user_input
+      document.getElementById("vote-count").textContent = user_input
+      // const votes = parseInt(votes.innerText)
+  })
+}
+
+function resetVoteCount(){
+  document.querySelector('#reset-btn').addEventListener('click', (e) => {
+      const votes = document.querySelector('#vote-count')
+      votes.innerText = "0"
+  })
+}
+
+function enterComment() {
+  document.getElementById('comment-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("Delish?")
+      let comment = event.target.comments.value
+    document.getElementById("foodComment").textContent = comment
+  })
+}
 
 function showRecipeBar(recipes) {
   // const span = document.createElement("span")
-  // debugger
+  
+
+  // removeAllChildNodes(recipeBarSpan)
   const pics = document.createElement('img')
   pics.src = recipes["strMealThumb"]
   pics.id = recipes["idMeal"]
   console.log("pics showing?")
   recipeBarSpan.appendChild(pics)
-  
+
+
 }  
+
+
 // Need to troubleshoot images constantly populating on click
 recipeBarSpan.addEventListener('click', (e)=> { 
     // e.preventDefault()
@@ -50,23 +89,23 @@ function displayRecipeImage(food) {
   console.log(food)
   foodImage.src = food.meals[0]["strMealThumb"]
   document.getElementById("recipeName").textContent = food.meals[0]["strMeal"]
-  document.getElementById("recipeIngredient").innerHTML = (`${food.meals[0]["strIngredient1"]}<br>
-    ${food.meals[0]["strIngredient2"]}<br>
-    ${food.meals[0]["strIngredient3"]}<br>
-    ${food.meals[0]["strIngredient4"]}<br>
-    ${food.meals[0]["strIngredient5"]}<br>
-    ${food.meals[0]["strIngredient6"]}<br>
-    ${food.meals[0]["strIngredient7"]}<br>
-    ${food.meals[0]["strIngredient8"]}<br>
-    ${food.meals[0]["strIngredient9"]}<br>
-    ${food.meals[0]["strIngredient10"]}<br>
-    ${food.meals[0]["strIngredient11"]}<br>
-    ${food.meals[0]["strIngredient12"]}<br>
-    ${food.meals[0]["strIngredient13"]}<br>
-    ${food.meals[0]["strIngredient14"]}<br>
-    ${food.meals[0]["strIngredient15"]}<br>
-    ${food.meals[0]["strIngredient16"]}<br>
-    ${food.meals[0]["strIngredient17"]}<br>
-    ${food.meals[0]["strIngredient18"]}<br>
-    ${food.meals[0]["strIngredient19"]}<br>
-    ${food.meals[0]["strIngredient20"]}`) }
+  document.getElementById("recipeIngredient").innerHTML = (`${food.meals[0]["strMeasure1"]} ${food.meals[0]["strIngredient1"]}<br>
+    ${food.meals[0]["strMeasure2"]} ${food.meals[0]["strIngredient2"]}<br>
+    ${food.meals[0]["strMeasure3"]} ${food.meals[0]["strIngredient3"]}<br>
+    ${food.meals[0]["strMeasure4"]} ${food.meals[0]["strIngredient4"]}<br>
+    ${food.meals[0]["strMeasure5"]} ${food.meals[0]["strIngredient5"]}<br>
+    ${food.meals[0]["strMeasure6"]} ${food.meals[0]["strIngredient6"]}<br>
+    ${food.meals[0]["strMeasure7"]} ${food.meals[0]["strIngredient7"]}<br>
+    ${food.meals[0]["strMeasure8"]} ${food.meals[0]["strIngredient8"]}<br>
+    ${food.meals[0]["strMeasure9"]} ${food.meals[0]["strIngredient9"]}<br>
+    ${food.meals[0]["strMeasure10"]} ${food.meals[0]["strIngredient10"]}<br>
+    ${food.meals[0]["strMeasure11"]} ${food.meals[0]["strIngredient11"]}<br>
+    ${food.meals[0]["strMeasure12"]} ${food.meals[0]["strIngredient12"]}<br>
+    ${food.meals[0]["strMeasure13"]} ${food.meals[0]["strIngredient13"]}<br>
+    ${food.meals[0]["strMeasure14"]} ${food.meals[0]["strIngredient14"]}<br>
+    ${food.meals[0]["strMeasure15"]} ${food.meals[0]["strIngredient15"]}<br>
+    ${food.meals[0]["strMeasure16"]} ${food.meals[0]["strIngredient16"]}<br>
+    ${food.meals[0]["strMeasure17"]} ${food.meals[0]["strIngredient17"]}<br>
+    ${food.meals[0]["strMeasure18"]} ${food.meals[0]["strIngredient18"]}<br>
+    ${food.meals[0]["strMeasure19"]} ${food.meals[0]["strIngredient19"]}<br>
+    ${food.meals[0]["strMeasure20"]} ${food.meals[0]["strIngredient20"]}`) }
