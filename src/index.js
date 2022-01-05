53b0fb0dfeda2cb951aa5d20f41c6f40ff243ab2
J Lee <jay.j.lee.1@gmail.com>
2022-01-05 11:43:43 -0500

    with some CSS

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Global Variables
 const recipeBar = document.getElementById('recipeBar');
 const recipeBarSpan = document.getElementById('recipeBarSpan');
 const recipeBox = document.getElementById('recipeBox');
@@ -5,51 +6,61 @@ const proteinBox = document.getElementById('proteinBox');
 const proteinInBox = document.querySelectorAll(".proteinInBox")
 
 
-document.querySelectorAll(".proteinInBox").forEach((ele) => {
+proteinInBox.forEach((ele) => {
   ele.addEventListener('click', (e)=> { 
+    // debugger
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.target.id}`)
     .then(res => res.json())
     .then(food => food.meals.forEach(showRecipeBar))
-      // {
-      // // debugger
-      // const pics = document.createElement('img')
-      // food.forEach(foodItem => {
-      //   pics.src = food["strMealThumb"]
-      //   recipeBar.appendChild(pics)
-      // })
-      // // pics.src = food["strMealThumb"].forEach()
-      // // recipeBar.appendChild(pics)
-      // } 
-      // )
+    // e.target.reset()
   })  
 })
 
 
 function showRecipeBar(recipes) {
   // const span = document.createElement("span")
+  // debugger
   const pics = document.createElement('img')
   pics.src = recipes["strMealThumb"]
   pics.id = recipes["idMeal"]
   console.log("pics showing?")
   recipeBarSpan.appendChild(pics)
-  
 }  
-// document.querySelectorAll("#recipeBarSpan").addEventListener('click', () => console.log("click"))
-document.getElementById("recipeBarSpan").addEventListener('click', (e)=> { 
-    
+// Need to troubleshoot images constantly populating on click
+recipeBarSpan.addEventListener('click', (e)=> { 
+    // e.preventDefault()
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${e.target.id}`)
     .then(res => res.json())
     .then(ele => displayRecipeImage(ele))
-    }
-
-
-
+    // e.target.reset()
+  }
+    
+    
 )
-
+// populating recipeBox
 function displayRecipeImage(food) {
   const foodImage = document.getElementById("recipeImage")
   console.log(food)
   foodImage.src = food.meals[0]["strMealThumb"]
   document.getElementById("recipeName").textContent = food.meals[0]["strMeal"]
-  document.getElementById("recipeIngredient").innerHTML = (`${food.meals[0]["strIngredient1"]}<br>${food.meals[0]["strIngredient2"]}`) }
+  document.getElementById("recipeIngredient").innerHTML = (`${food.meals[0]["strIngredient1"]}<br>
+    ${food.meals[0]["strIngredient2"]}<br>
+    ${food.meals[0]["strIngredient3"]}<br>
+    ${food.meals[0]["strIngredient4"]}<br>
+    ${food.meals[0]["strIngredient5"]}<br>
+    ${food.meals[0]["strIngredient6"]}<br>
+    ${food.meals[0]["strIngredient7"]}<br>
+    ${food.meals[0]["strIngredient8"]}<br>
+    ${food.meals[0]["strIngredient9"]}<br>
+    ${food.meals[0]["strIngredient10"]}<br>
+    ${food.meals[0]["strIngredient11"]}<br>
+    ${food.meals[0]["strIngredient12"]}<br>
+    ${food.meals[0]["strIngredient13"]}<br>
+    ${food.meals[0]["strIngredient14"]}<br>
+    ${food.meals[0]["strIngredient15"]}<br>
+    ${food.meals[0]["strIngredient16"]}<br>
+    ${food.meals[0]["strIngredient17"]}<br>
+    ${food.meals[0]["strIngredient18"]}<br>
+    ${food.meals[0]["strIngredient19"]}<br>
+    ${food.meals[0]["strIngredient20"]}`) }
 
