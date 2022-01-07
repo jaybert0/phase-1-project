d6a7d1047ad9ef71a59a70c44282f35b0c596f6e
J Lee <jay.j.lee.1@gmail.com>
2022-01-07 11:12:34 -0500

    Final 1.0 - working on centering images in bar span

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ function init() {
   resetVoteCount()
   enterComment()
 }
-
 document.addEventListener("DOMContentLoaded", init)
 
 // Global Variables
@@ -16,13 +15,10 @@ const proteinInBox = document.querySelectorAll(".proteinInBox")
 // initial fetch and populating of thumbnail bar
 proteinInBox.forEach((ele) => {
   ele.addEventListener('click', (e)=> { 
-    // debugger
-    
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${e.target.id}`)
     .then(res => res.json())
     .then(food => food.meals.forEach(showRecipeBar))
     .finally(removeAllChildNodes(recipeBarSpan))
-    // e.target.reset()
   })  
 })
 function removeAllChildNodes(parent) {
@@ -31,76 +27,58 @@ function removeAllChildNodes(parent) {
     }
 }
 function showRecipeBar(recipes) {
-  // const span = document.createElement("span")
-    // removeAllChildNodes(recipeBarSpan)
   const pics = document.createElement('img')
   pics.src = recipes["strMealThumb"]
   pics.id = recipes["idMeal"]
-  console.log("pics showing?")
   recipeBarSpan.appendChild(pics)
 }  
+
+// populating individual recipe after clicking on thumbnail
 recipeBarSpan.addEventListener('click', (e)=> { 
-    // e.preventDefault()
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${e.target.id}`)
     .then(res => res.json())
     .then(ele => displayRecipeImage(ele))
-    // e.target.reset()
-  }
-    
-    
+  }   
 )
-
-// populating individual recipe after clicking on thumbnail
 function displayRecipeImage(food) {
   const foodImage = document.getElementById("recipeImage")
   console.log(food)
+  // For youtube embed including extracting video id
   let youtubeLink = food.meals[0]["strYoutube"]
   function getId(url) {
     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
     const match = url.match(regExp);
-    
+    console.log(regExp)
+    console.log(match)
     return (match && match[2].length === 11)
     ? match[2]
     : null;
   }
   let newYoutube = getId(youtubeLink)
-  console.log("https://www.youtube.com/embed/"+newYoutube)
+
   foodImage.src = food.meals[0]["strMealThumb"]
   document.getElementById("recipeName").textContent = food.meals[0]["strMeal"]
   document.getElementById("recipeInstructions").textContent = food.meals[0]["strInstructions"]
   document.getElementById("youtube").src = "https://www.youtube.com/embed/"+newYoutube
+    // filtering for null/undefined/""/" "
     let noNull = Object.fromEntries(Object.entries(food.meals[0]).filter(([_, v]) => v != null && v != '' && v != ' '))
-      console.log("noNull")
-      console.log(noNull)
     let noNullIngredients = Object.fromEntries(Object.entries(noNull).filter(([key]) => key.includes('strIngredient')))
-      console.log("Ingredients")
-      console.log(noNullIngredients)
     let noNullMeasurements = Object.fromEntries(Object.entries(noNull).filter(([key]) => key.includes('strMeasure')))
-      console.log('Measurements')
-      console.log(noNullMeasurements)
     let ingArr = Object.values(noNullIngredients)
-      console.log("Ingredient Values")
-      console.log(ingArr)
     let meaArr = Object.values(noNullMeasurements)
-      console.log("Measurements Values")
-      console.log(meaArr)
-  let ingList = ""
-  for(let i=0; i<ingArr.length; i++) {
-    ingList += '<li>' + meaArr[i] + ' ' + ingArr[i]; 
-  }
-    ingList = '<ul>' + ingList +  '</ul>'
+    let ingList = ""
+      for(let i=0; i<ingArr.length; i++) {
+        ingList += '<li>' + meaArr[i] + ' ' + ingArr[i]; 
+      }
+        ingList = '<ul>' + ingList +  '</ul>'
   document.getElementById("recipeIngredient").innerHTML = ingList
-    
-  
 }
 function handleForm(){
   document.getElementById('votes-form').addEventListener('submit', (event) => {
     event.preventDefault();
     console.log("Thanks for the rating")
       let user_input = event.target.votes.value
-      // document.querySelector('#votes-form').innerText = user_input
       document.getElementById("vote-count").textContent = user_input
-      // const votes = parseInt(votes.innerText)
   })
 }
 function resetVoteCount(){
